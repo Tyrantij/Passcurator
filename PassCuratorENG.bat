83ebba17de5b6c352e312c33f5e197cc3c8c900d
Tyrantij <79326997+Tyrantij@users.noreply.github.com>
2024-10-26 18:39:56 +0300

    Add files via upload

---
+++ PassCuratorENG.bat
@@ -0,0 +1,31 @@
+@echo off
+
+title passcurator beta
+color 0e                                                  
+echo P	A	S	S	C	U	R	A	T	O	R   
+echo =================================================================================
+echo                                version: 0.0.1
+echo =================================================================================
+echo 1 - Show accounts
+echo 2 - Add account
+echo 0 - Close
+echo =================================================================================
+set /P var= Choose action: 
+if %var%==1 goto show
+if %var%==2 goto add  
+if %var%==0 exit
+
+:show
+echo SERVICE USERNAME PASSWORD
+echo =================================================================================
+type passwords.txt
+pause
+exit
+
+:add 
+set /P service= Enter service(if dont has enter '-'): 
+set /P username= Enter username: 
+set /P password= Enter password: 
+echo %service% %username% %password% >> passwords.txt
+exit
+
